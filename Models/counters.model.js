7055a01895f8fc54b96bb86f81532b3814c48da7
Rokaya Elziady <rokayaelziady@gmail.com>
2020-12-16 10:45:38 +0200

    delete folders

--- Models/counters.model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-mongoose.set('useCreateIndex', true);
-var Schema = mongoose.Schema
-var counter = new Schema({
-academicCount:{type:Number,default:0},
-hrCount:{type:Number,default:0},
-})
-var counterModel = mongoose.model('counter', counter);
-module.exports = counterModel
