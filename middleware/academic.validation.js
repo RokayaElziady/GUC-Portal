5e8b319414146e2101a5d75b01b9136855d2569e
nadine mohamed <nano_moh98@yahoo.com>
2020-12-25 14:29:06 +0200

    kk2

--- middleware/academic.validation.js
+++ middleware/academic.validation.js
@@ -7,7 +7,7 @@ const validatePostacademic = (req, res, next) => {
     officeLocation:Joi.string(),
     extraInformation:Joi.string(),
     gender:Joi.string().required().valid('female','male'),
-    role:Joi.string().required().valid(coordinator,HOD,DOC,TA),
+    role:Joi.string().required().valid('coordinator','HOD','DOC','TA'),
     dayOff:Joi.string().valid('Saturday','Sunday','Monday','Tuesday','Wednesday' ,'Thursday','Friday'),
     department:Joi.string().required()  
     })
@@ -27,7 +27,7 @@ const validatePostacademic = (req, res, next) => {
     officeLocation:Joi.string(),
     extraInformation:Joi.string(),
     gender:Joi.string().valid('female','male'),
-    role:Joi.string().valid(coordinator,HOD,DOC,TA),
+    role:Joi.string().valid('coordinator','HOD','DOC','TA'),
     dayOff:Joi.string().valid('Saturday','Sunday','Monday','Tuesday','Wednesday' ,'Thursday','Friday'),
    
     })
