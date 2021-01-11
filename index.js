5bb6c5adf6c18aeb8992a08d93a59276de20f4ad
ROKAYA <rokayaelziady@gmail.com>
2021-01-11 18:45:44 +0200

    a

--- index.js
+++ index.js
@@ -72,14 +72,17 @@ app.use('/',(req,res,next)=>{
 })
 
 app.use(cors(corsO))
-app.UseCors(options => options.AllowAnyOrigin());
+//app.UseCors(options => options.AllowAnyOrigin());
 app.use('/logging',log)
 if(process.env.NODE_ENV==='production'){
+  app.use(express.static('frontend/build'))
+
+  
   app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
   })
   
-  app.use(express.static('frontend/build'))
+  
  
   
 }
