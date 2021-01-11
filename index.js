93ea9b1b26bbfeb7e81aed33f4d1049a15866eac
ROKAYA <rokayaelziady@gmail.com>
2021-01-11 20:57:12 +0200

    aaa

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.use('/',(req,res,next)=>{
 })
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, "./client", "build")))
+app.use(express.static(path.join(__dirname, "/client", "build")))
 //app.UseCors(options => options.AllowAnyOrigin());
 app.use('/logging',log)
 
@@ -103,7 +103,7 @@ if(process.env.NODE_ENV==='production'){
 
 
   app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    res.sendFile(path.resolve(__dirname,'/client','build','index.html'))
   })
 }
 
