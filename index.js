176ae4ce24a8ba150f350e7347c82dc435e202ed
ROKAYA <rokayaelziady@gmail.com>
2021-01-11 20:42:59 +0200

    kkk

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.use('/',(req,res,next)=>{
 })
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "./client", "build")))
 //app.UseCors(options => options.AllowAnyOrigin());
 app.use('/logging',log)
 
