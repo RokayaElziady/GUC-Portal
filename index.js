d396bf3d3e4e353d5554d45deea4d9e292a9183a
ROKAYA <rokayaelziady@gmail.com>
2021-01-11 17:02:28 +0200

    q

--- index.js
+++ index.js
@@ -106,11 +106,12 @@ const a =new hrmodel({
 //a.save()
 
 if(process.env.NODE_ENV==='production'){
-  
-  app.use(express.static('frontend/build'))
-  app.get('*',(req,res)=>{
+  app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
   })
+  
+  app.use(express.static('frontend/build'))
+ 
   
 }
 
