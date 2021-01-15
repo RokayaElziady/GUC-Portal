b17262187b93d022bb108e4c4c48b4a2069f4a58
ROKAYA <rokayaelziady@gmail.com>
2021-01-15 22:14:48 +0200

    ssss

--- client/keys_dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  backendLink: 'http://localhost:5000',
-}
--- client/src/globalState/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import {
-  tokenReducer,
-} from './SigninReducer'
-
-export const root = combineReducers({
-  token: tokenReducer,
-})
